2e1e824776c6697e72e7e354f480587c177a3d95
William Long <wlong57@gmail.com>
2019-09-02 21:16:04 -0400

    Update Sin_Cos.py

--- Sin_Cos.py
+++ Sin_Cos.py
@@ -12,7 +12,7 @@ import numpy as np
 
 
 X = np.arange(0,2*np.pi,0.1)
-Y = np.sin(X)
+Y = np.sin(X) #add
 Z = np.cos(X)
 b = 5 # cutoff for the tan graph. Otherwise, the y axis is much too big.
 T = [min(b, max(-b, np.tan(x))) for x in X]
