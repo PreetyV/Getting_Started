8c47abb0133fd0d426e8d89035aa7a719a772631
PreetyV <54750544+PreetyV@users.noreply.github.com>
2019-11-23 16:11:44 -0500

    updated method

--- Sin_Cos.py
+++ Sin_Cos.py
@@ -11,19 +11,21 @@ import numpy as np
 # In[15]:
 
 
-X = np.arange(0,2*np.pi,0.1)
-Y = np.sin(X) #add
-Z = np.cos(X)
-b = 5 # cutoff for the tan graph. Otherwise, the y axis is much too big.
-T = [min(b, max(-b, np.tan(x))) for x in X]
+def plotstart():
+  X = np.arange(0,2*np.pi,0.1)
+  Y = np.sin(X) #add
+  Z = np.cos(X)
+  b = 5 # cutoff for the tan graph. Otherwise, the y axis is much too big.
+  T = [min(b, max(-b, np.tan(x))) for x in X]
+  plt.plot(X,Y,X,Z,X,T)
+  plt.show()
+
 
 
 # In[16]:
 
-
-plt.plot(X,Y,X,Z,X,T)
-plt.show()
-
+if __name__ == '__main__':
+    plotstart()
 
 # In[ ]:
 
